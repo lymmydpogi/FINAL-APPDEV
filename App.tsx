3725e2f68ba97ff64709f8fc13f9e1c5775d67e4
ralph john <bossrj12@gmail.com>
2026-05-22 03:13:46 +0800

    feat(app): wire providers bootstrap and navigation

--- App.tsx
+++ App.tsx
@@ -1,23 +1,44 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import AppNav from './src/navigations';
-
+import BootstrapGate from './src/components/BootstrapGate';
+import { ToastProvider } from './src/context/ToastContext';
+import AppNav from './src/navigations/index';
 import rootSaga from './src/app/sagas';
 import configureStore from './src/app/reducers';
-import { Provider } from 'react-redux';
+import { initializeGoogleSignIn } from './src/auth/googleSignIn';
+import { theme } from './src/theme/tokens';
 
-const { store, runSaga } = configureStore();
+const { store, persistor, runSaga } = configureStore();
 runSaga(rootSaga);
 
 const App = () => {
+  useEffect(() => {
+    initializeGoogleSignIn();
+  }, []);
+
   return (
     <Provider store={store}>
-      <View style={{ flex: 1 }}>
-        <AppNav />
-      </View>
+      <SafeAreaProvider>
+        <ToastProvider>
+          <BootstrapGate persistor={persistor}>
+            <View style={styles.root}>
+              <AppNav />
+            </View>
+          </BootstrapGate>
+        </ToastProvider>
+      </SafeAreaProvider>
     </Provider>
   );
 };
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: theme.colors.background,
+  },
+});
 
 export default App;
